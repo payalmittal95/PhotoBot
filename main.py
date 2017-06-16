4494226b4a537574a66d33349d90db0fd1101024
Payal Mittal <payal1995mittal@gmail.com>
2017-06-17 00:03:24 +0530

    Nw function added to get user id via username

--- main.py
+++ main.py
@@ -20,4 +20,19 @@ def self_info():
         print "Status code other than 200 received."
         print user_info['meta']['code']
 
-self_info()
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+#self_info()
+print get_user_id("payal.mittal95")
