2b56c4ba7dd22f26a636ddb92876f4ac9a3ddd80
Payal Mittal <payal1995mittal@gmail.com>
2017-06-20 16:39:45 +0530

    function added to get a user's information by their username

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#importing access token from keys file and requests library
 from keys import ACCESS_TOKEN
-import requests
+import requests, urllib
 BASE_URL = "https://api.instagram.com/v1/"
 
 #Defining function for our own info
@@ -20,8 +21,9 @@ def self_info():
         print "Status code other than 200 received."
         print user_info['meta']['code']
 
+#Function to get a user's id by their username
 def get_user_id(insta_username):
-    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
 
@@ -32,7 +34,25 @@ def get_user_id(insta_username):
             return None
     else:
         print 'Status code other than 200 received!'
-        exit()
+        print "status code : %d" % user_info['meta']['code']
+        return None
 
-#self_info()
-print get_user_id("payal.mittal95")
+#function to get a user's information by their user-name
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print "User doesn't exist."
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, ACCESS_TOKEN)
+    print "Requesting url : %s" % request_url
+    user_info = requests.get(request_url).json()
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else :
+            print "There is no data for this user."
+    else :
+        print "Status code other than 200 recieved.[%d]" % user_info['meta']['code']
